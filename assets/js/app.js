fd8747d5d46d2866171deb3c57a61058230470d8
Goofball441 <wombat2017@gmail.com>
2022-04-30 14:09:20 -0400

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+  // canvas and svg width, height, and margins
+  const svgWidth = 900
+  const svgHeight = 450
+  const margin = {
+      top: 25,
+      right: 50,
+      bottom: 130,
+      left: 90
+    }
+  const width = svgWidth - margin.left - margin.right
+  const height = svgHeight - margin.top - margin.bottom
+  
+  // canvas
+  const svg = d3.select('#scatter').append('svg').attr('width', svgWidth).attr('height',svgHeight)
+  
+  // chartGroup
+  const chartGroup = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`)
+  
+  // data
+  const dataFile = 'assets/data/data.csv'
+  d3.csv(dataFile).then(visualization)
+  
+  // plot function
+  function visualization(states) {
+    
+    // data loop
+    states.map(function (data) {
+      data.poverty = +data.poverty
+      data.obesity = +data.obesity
+      data.income = +data.income
+    })
+  
+    // scale fnxs with min and max (plus a little more space)
+    const linearScaleX = d3.scaleLinear().domain([8,d3.max(states,(d,i)=>d.poverty+0.5)]).range([0, width])
+    const linearScaleY = d3.scaleLinear().domain([20,d3.max(states,(d,i)=>d.obesity+1)]).range([height, 0])
+  
+    // call scale fnx with axes fnxs with set tick marks on x axis
+    const bottomAxis =  d3.axisBottom(linearScaleX).ticks(10)
+    const leftAxis =  d3.axisLeft(linearScaleY)
+  
+    // append axes to chartGroup
+    chartGroup.append('g').attr('transform', `translate(0, ${height})`).call(bottomAxis)
+    chartGroup.append('g').call(leftAxis)
+  
+    // label axes
+    chartGroup.append('text').attr('transform','rotate(-90)').attr('x',0-height/2).attr('y',0-margin.left+40)
+      .attr('dy','1em').attr('class','aText').text('Obese (%)')
+  
+    chartGroup.append('text').attr('transform',`translate(${width/2},${height+margin.top+20})`).attr('class','aText').text('In Poverty (%)')
+    
+    // Define the domain extent
+    let extent =d3.extent(states , function(d){return d.income});
+    
+    //Define colour scale
+      selectedColor = d3['interpolateRdBu'];
+      let colorScale = d3.scaleSequential(selectedColor)
+      .domain(extent);
+  
+    // create plot markers
+    let circlesGroup = chartGroup.selectAll('circle').data(states).enter().append('circle')
+      .attr('cx',d=>linearScaleX(d.poverty)).attr('cy',d=>linearScaleY(d.obesity))
+      .attr('r','17').attr('fill',function(d){return colorScale(d.income)}).attr('opacity','0.85')
+      
+    // add state abbreviations to circles (centered on both x and y of circle)
+    circlesGroup = chartGroup.selectAll().data(states).enter().append('text')
+      .attr('x',d=>linearScaleX(d.poverty)).attr('y',(d,i)=>linearScaleY(d.obesity)+4)
+      .style('font-size','15px').style('text-anchor','middle').style('fill','white').text(d=>(d.abbr))
+  
+    // make tooltip
+    const toolTip = d3.tip().attr('class','d3-tip').offset([80,-65])
+      .html(function(d){return(`<strong>${d.state}</strong><br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}%<br>Income: $${d.income}`)})
+  
+    // add tooltip to chart
+    chartGroup.call(toolTip)
+  
+    // tooltip event listeners (show/hide)
+    circlesGroup.on('mouseover',function(data){toolTip.show(data,this)})
+      .on('mouseout',function(data){toolTip.hide(data)})
+    
+    //create legend
+    const linearGradient = chartGroup.append("defs")
+                              .append("linearGradient")
+                              .attr("id", "linear-gradient");
+                              linearGradient.selectAll("stop")
+                    .data(colorScale.ticks()
+                    .map((t, i, n) => ({ 
+                      offset: `${100*i/n.length}%`, 
+                      color: colorScale(t) })))
+                    .enter()
+                      .append("stop")
+                      .attr("offset", d => d.offset)
+                      .attr("stop-color", d => d.color);
+        chartGroup.append("rect")
+       .attr('transform', `translate(150,360)`)
+       .attr("width", 400)
+       .attr("height", 20)
+       .style("fill", "url(#linear-gradient)");
+      const colorAxis = d3.axisBottom(d3.scaleLinear()
+                          .domain(colorScale.domain())
+                          .range([0,400]))
+                          .ticks(5).tickSize(-20);
+      chartGroup.append('g').call(colorAxis)
+       .attr('class','colorLegend')
+       .attr('transform','translate(150,380)')
+       .selectAll('text')
+       .style('text-anchor','end')
+       .attr('dx','-10px')
+       .attr('dy', '0px')
+       .attr('transform','rotate(-45)');
+  }
+  
